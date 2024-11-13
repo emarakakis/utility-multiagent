4f0a7198ee1e8dc2e8a96655d6af1888f6620b2b
emarakakis <eftihismarak@gmail.com>
2024-11-13 22:29:26 +0200

    Started on the radio selectors, and forms

--- myenv/app.py
+++ myenv/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
+    if request.method == 'POST':
+        test_input = request.form.get('test-input')  # Gets the value of 'test-input' from the form
+        print(f"{test_input}") 
     return render_template('simple.html')
 
 if __name__ == '__main__':
